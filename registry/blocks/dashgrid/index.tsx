25f0c05543516086a9a9de64fdc0fc0711af2bcf
Jeff Kinley <jkinley@icloud.com>
2025-05-29 14:06:04 -0700

    fix path

--- registry/blocks/dashgrid/index.tsx
+++ registry/blocks/dashgrid/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { useUsers } from './hooks/useUsers';
+import { useUsers } from './hooks/use-users';
 
 export const Route = createFileRoute('/dashgrid/')({
   component: DashGrid
