ab98c8441ae8d90c96408fd1b075351b7153f2a8
Jeff Kinley <jkinley@icloud.com>
2025-05-29 08:41:26 -0700

    update

--- registry/blocks/dashgrid/page.tsx
+++ registry/blocks/dashgrid/page.tsx
@@ -1,4 +1,10 @@
-export default function Dashgrid() {
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/dashgrid')({
+  component: Dashgrid
+});
+
+function Dashgrid() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <h1>Welcome to Dashgrid</h1>
