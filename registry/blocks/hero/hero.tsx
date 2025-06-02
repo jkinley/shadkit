a28a87ab5c158b82ea024265af3cee390a0fc724
Jeff Kinley <jkinley@icloud.com>
2025-06-02 09:21:04 -0700

    fix theme

--- registry/blocks/hero/hero.tsx
+++ registry/blocks/hero/hero.tsx
@@ -6,9 +6,7 @@ const Hero = () => {
   return (
     <div className="py-12 grid gap-4 place-items-center">
       <div className="text-center max-w-2xl">
-        <Badge className="bg-gradient-to-br via-70% from-primary via-muted/30 to-primary rounded-full py-1 border-none">
-          Just released v1.0.0
-        </Badge>
+        <Badge className="bg-primary rounded-full py-1 border-none">Just released v1.0.0</Badge>
         <h1 className="mt-6 text-4xl sm:text-5xl md:text-6xl md:leading-[1.2] font-bold">
           Customized Shadcn UI Blocks & Components
         </h1>
@@ -17,7 +15,7 @@ const Hero = () => {
           development workflow with easy-to-implement examples.
         </p>
         <div className="mt-12 flex items-center justify-center gap-4">
-          <Button size="lg" className="rounded-full text-base">
+          <Button size="lg" className="bg-primary rounded-full text-base">
             Get Started <ArrowUpRight className="!h-5 !w-5" />
           </Button>
           <Button variant="outline" size="lg" className="rounded-full text-base shadow-none">
