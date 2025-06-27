21149669d28b55d610c887203d9e05f7a93a8195
Jeff Kinley <jkinley@icloud.com>
2025-06-27 09:15:43 -0700

    fix

--- src/routes/_main-layout/dashgrid.tsx
+++ src/routes/_main-layout/dashgrid.tsx
@@ -1,9 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import DashGrid from '@/registry/blocks/dashgrid';
 
 export const Route = createFileRoute('/_main-layout/dashgrid')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_main-layout/dashgrid"!</div>
-}
+  component: DashGrid
+});
